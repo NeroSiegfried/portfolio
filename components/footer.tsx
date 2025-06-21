3b6924982e1b2f190b563b882e1f2682a7ae2c15
Victor Nabasu <121029407+NeroSiegfried@users.noreply.github.com>
2025-06-22 00:11:34 +0100

    Removed my twitter links

--- components/footer.tsx
+++ components/footer.tsx
@@ -201,28 +201,6 @@ export default function Footer() {
                 <circle cx="4" cy="4" r="2"></circle>
               </svg>
             </a>
-            <a
-              href="https://twitter.com/NeroSiegfried"
-              className="text-primary-foreground/80 hover:text-primary-foreground transition-colors"
-              aria-label="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-twitter"
-              >
-                <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-              </svg>
-            </a>
           </div>
         </div>
 
